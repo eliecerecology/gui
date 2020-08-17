49a81d7351e075ed2d235340a50539a96cff192e
eliecer.diaz <eliecer.diaz@cgi.com>
2020-08-17 21:23:01 +0300

    deletion

--- decorator.py
+++ decorator.py
@@ -14,26 +14,3 @@ pretty()
 
 #extraordinary
 
-#parameters
-def divide(a,b):
-    return a/b
-
-#division = divide(a,b)
-
-division = divide(a = 5, b = 2)
-
-def smart_divide(func):
-    def inner(a, b):
-        print("I am going to divide", a, "and", b)
-        if b == 0:
-            print("Whoops! cannot divide")
-            return
-
-        return func(a, b)
-    return inner
-
-@smart_divide
-def divide(a, b):
-    print(a/b)
-
-divide(2, 5)
